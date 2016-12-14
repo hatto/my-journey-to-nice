123d63f15ad604d4eacf08f82abd45301ee7313b
peter kracik <peter@kracik.sk>
2016-12-14 22:49:19 +0100

    instagram

--- src/js/components/Instagram.jsx
+++ src/js/components/Instagram.jsx
@@ -1,9 +1,14 @@
 import React        from 'react';
 import classNames   from 'classnames';
 import moment       from 'moment';
+import Masonry      from 'react-masonry-component';
 
 import InstagramStore   from '../stores/InstagramStore';
 import InstagramPost    from './InstagramPost.jsx';
+
+var masonryOptions = {
+    transitionDuration: 0
+};
 
 /**
  * get actual values from the store
@@ -41,11 +46,28 @@ var Instagram = React.createClass({
 
     getPosts: function() {
         if (this.state.photos.length) {
-            let rows = [];
-            let index = 0;
+            let rows = [],
+                index = 0,
+                colors = ['white', 'red', 'black']
+                ;
             for (let photo of this.state.photos) {
                 index++;
-                rows.push( <InstagramPost key={ index } data={photo} /> );
+                let size = (index%3 == 0) ? 'portrait' : 'landscape',
+                    position = 'top',
+                    cls = classNames([
+                        'instagram__item',
+                        'instagram__item--masonry',
+                        'instagram__item--' + size,
+                        'instagram__item--' + position,
+                        'instagram__item--' + colors[Math.floor(Math.random()*colors.length)]
+                    ])
+                    ;
+
+                rows.push(
+                    <div key={ index } className={ cls }>
+                        <InstagramPost data={photo} />
+                    </div>
+                );
             }
             return rows;
         }
@@ -54,8 +76,13 @@ var Instagram = React.createClass({
 
     render: function() {
         return (
-            <div className="instagram">
-                { this.getPosts() }
+            <div className="wrap">
+                <div className="instagram">
+                    <p className="instagram__title">From my instagram.</p>
+                    <div className="instagram__wrap">
+                        { this.getPosts() }
+                    </div>
+                </div>
             </div>
         );
     }
--- src/js/components/InstagramPost.jsx
+++ src/js/components/InstagramPost.jsx
@@ -8,12 +8,26 @@ import classNames   from 'classnames';
  */
 var InstagramPost = React.createClass({
 
+    highlightHashtags: function(text) {
+        return this.nl2br(text.replace(/(^|\s)(#[a-z\d-]+)/ig, "$1<strong>$2</strong>"));
+    },
+
+    nl2br: function(str) {
+        var breakTag = '<br />';
+        return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
+    },
+
     getCaption: function(photo) {
         if (photo.caption) {
+            let cls = classNames({
+                'instagram-post__content-text': true,
+                'instagram-post__content-text--small': photo.caption.text.length > 150
+            });
             return (
-                <div>
-                    { photo.caption.text }
-                </div>
+                <div
+                    className={ cls }
+                    dangerouslySetInnerHTML={{ __html: this.highlightHashtags(photo.caption.text) }}
+                />
             );
         }
         return null;
@@ -23,12 +37,16 @@ var InstagramPost = React.createClass({
         let photo = this.props.data;
         return (
             <div className="instagram-post">
-                <img
-                    src={ photo.images.standard_resolution.url }
-                    width={ photo.images.standard_resolution.width }
-                    height={ photo.images.standard_resolution.height }
-                />
-                { this.getCaption(photo) }
+                <div className="instagram-post__image">
+                    <img
+                        src={ photo.images.standard_resolution.url }
+                        width={ photo.images.standard_resolution.width }
+                        height={ photo.images.standard_resolution.height }
+                    />
+                </div>
+                <div className="instagram-post__content">
+                    { this.getCaption(photo) }
+                </div>
             </div>
         );
     }
